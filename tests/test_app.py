4fb026bb2ed417c2d3c4c58b77666bd55ef93ba2
zhangyingnan11111 <147327988+zhangyingnan11111@users.noreply.github.com>
2025-10-10 13:27:15 +0800

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,6 +12,6 @@ class TestApp(unittest.TestCase):
         # 测试根路由
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode('utf-8'), 'Hello, World!')
+        self.assertEqual(response.data.decode('utf-8'), 'Hello, NEU!')
 if __name__ == '__main__':
     unittest.main()
